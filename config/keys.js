e1e0d911c137af0087371679669affc6095cef4b
Teja Kumar Reddy Kovvuri <tkovvuri@deakin.edu.au>
2020-01-29 20:13:24 +1100

    commit -m "new mongodb atlas account created"

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 //dbPassword='mongodb://localhost:27017/NISTEEDB'
-dbPassword= "mongodb+srv://ktkr:kittu9013@ktkr-ed3fc.mongodb.net/test?retryWrites=true&w=majority";
+dbPassword="mongodb+srv://SIT782:SIT7a8b2c@cluster0-i6itg.mongodb.net/test?retryWrites=true&w=majority";
 module.exports = {
     mongoURI: dbPassword
 };
