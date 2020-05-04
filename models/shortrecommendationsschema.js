4bc84829ee5789b1017347a39b43d4622dd90ec7
tkovvuri <tkovvuri@deakin.edu.au>
2020-05-04 13:02:24 +1000

    short recommendations fixed

--- models/shortrecommendationsschema.js
+++ models/shortrecommendationsschema.js
@@ -25,7 +25,6 @@ var Rec = module.exports = mongoose.model('shortrecommendations', recSchema); //
 module.exports.addRec = function(recs, callback) {
   Rec.create(recs, callback);
 }
-
 module.exports.getRec = function(_id, callback) {
       Rec.findById(_id, callback);
 }
